81499f0dc7ab722ffe283d992fe7193fec9c6508
github username <github email address>
2018-04-25 20:46:03 -0400

    post new message to db works

--- routes/messages.js
+++ routes/messages.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const knex = require("../db/knex")
 
 // get messages http://localhost:3000/messages
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   knex("message")
     .select()
     .then(messages => {
@@ -11,8 +11,24 @@ router.get("/", function(req, res, next) {
     })
 })
 
-router.get("/new", function(req, res, next) {
+router.get("/new", (req, res, next) => {
   res.render("new")
+})
+
+router.post("/", (req, res) => {
+  console.log("BODY: ", req.body)
+  let message = {
+    content: req.body.content,
+    countrycode: req.body.countrycode,
+    phone: req.body.phone,
+    email: req.body.email
+  }
+  knex("message")
+    .insert(message, "*")
+    .then(messages => {
+      message = messages[0]
+      res.redirect(`/message/${message.id}`)
+    })
 })
 
 module.exports = router
